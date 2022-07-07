3024ab7f9bfa36c13ac3d714f94e39d9a3d1e326
Neil Mastroianni <nmastroianni@longbranch.k12.nj.us>
2022-07-07 12:43:29 -0400

    adding chakra-ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "../styles/globals.css";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      h2: {
+        fontWeight: "800",
+      },
+      h3: {
+        fontWeight: "600",
+      },
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
