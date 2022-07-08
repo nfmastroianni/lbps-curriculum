54f84134403174e74d447f2dee8abe682a35a492
Neil Mastroianni <suancho@gmail.com>
2022-07-08 07:59:21 -0400

    removing console log

--- libs/sheets.js
+++ libs/sheets.js
@@ -4,7 +4,6 @@ export async function fetchCurricula() {
   try {
     const target = ['https://www.googleapis.com/auth/spreadsheets']
     const keys = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEYS)
-    console.log('KEYS ====> ', keys)
     const privateKey = keys.private_key.replace(new RegExp('\\\\n', 'g'), '\n')
     const client = new google.auth.JWT(
       keys.client_email,
@@ -36,44 +35,4 @@ export async function fetchCurricula() {
     console.log(err)
   }
   return []
-
-  // try {
-  //   const client = new google.auth.JWT(
-  //     process.env.CLIENT_EMAIL,
-  //     null,
-  //     process.env.PRIVATE_KEY,
-  //     ['https://www.googleapis.com/auth/spreadsheets']
-  //   )
-
-  //   client.authorize(async function (err, tokens) {
-  //     if (err) {
-  //       return res.status(400).send(JSON.stringify({ error: true }))
-  //     }
-
-  //     const gsapi = google.sheets({ version: 'v4', auth: client })
-
-  //     const opt = {
-  //       spreadsheetId: '1u-p159g3E8lue4fznYOnEmjUDWYV2b9Li92GbRljJVQ',
-  //       range: 'Sheet1!A1:G',
-  //     }
-  //     let response = await gsapi.spreadsheets.values.get(opt)
-  //     const rows = response.data.values
-  //     rows.shift()
-  //     if (rows.length) {
-  //       const curricula = rows.map((row) => ({
-  //         published: row[0],
-  //         title: row[1],
-  //         span: row[2],
-  //         level: row[3],
-  //         area: row[4],
-  //         guide: row[5],
-  //         calendar: row[6],
-  //       }))
-  //       return curricula
-  //     }
-  //   })
-  // } catch (e) {
-  //   console.log(e)
-  // }
-  // return []
 }
