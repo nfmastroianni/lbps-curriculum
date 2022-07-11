1108183b832faee2b070c900606996e2cc215fe1
Neil Mastroianni <suancho@gmail.com>
2022-07-11 05:13:50 -0400

    preparing for side drawer

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,6 +1,18 @@
-import Headroom from "react-headroom";
-import styled from "@emotion/styled";
-import Image from "next/image";
+import Headroom from 'react-headroom'
+import styled from '@emotion/styled'
+import Image from 'next/image'
+import { useRef } from 'react'
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+} from '@chakra-ui/react'
+import { Show } from '@chakra-ui/react'
 
 const Nav = styled.nav`
   padding: 1rem;
@@ -15,26 +27,37 @@ const Nav = styled.nav`
     font-size: 1.1rem;
     margin-left: 0.5rem;
   }
+  img {
+    display: none;
+  }
   @media (min-width: 640px) {
+    img {
+      display: inline;
+    }
     h1 {
       font-size: 1.4rem;
       margin-left: 0.8rem;
     }
   }
-`;
+`
 
 export default function Navbar(props) {
   return (
     <Headroom>
       <Nav>
-        <Image
-          src="/images/curriculum_logo.png"
-          height={50}
-          width={50}
-          alt="Long Branch Public Schools Seal"
-        />
-        <h1>LBPS Curriculum Warehouse</h1>
+        <Show above='lg'>
+          <Image
+            src='/images/lbps_logo.png'
+            height={50}
+            width={50}
+            alt='Long Branch Public Schools Seal'
+          />
+        </Show>
+        <Show below='lg'>
+          <p>HIDE ABOVE MD</p>
+        </Show>
+        <h1>LBPS Curriculum &amp; Instruction</h1>
       </Nav>
     </Headroom>
-  );
+  )
 }
