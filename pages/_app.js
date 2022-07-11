6f570608d43ad0c7962a0312e26bcb07eefe69de
Neil Mastroianni <nmastroianni@longbranch.k12.nj.us>
2022-07-11 11:36:52 -0400

    adding brand colors

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,34 @@
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import "../styles/globals.css";
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import '../styles/globals.css'
 
 const theme = extendTheme({
+  colors: {
+    brand: {
+      100: '#002333',
+      200: '#0d4c38',
+      300: '#cad5e2',
+      400: '#cff5ce',
+      500: '#0d4c38',
+    },
+  },
   styles: {
     global: {
       h2: {
-        fontWeight: "800",
+        fontWeight: '800',
       },
       h3: {
-        fontWeight: "600",
+        fontWeight: '600',
       },
     },
   },
-});
+})
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
