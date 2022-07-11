d6ed4bdf6902bf07defe1fba40df8ef2c909df32
Neil Mastroianni <nmastroianni@longbranch.k12.nj.us>
2022-07-11 11:37:05 -0400

    adding hero section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
 import Layout from '../components/Layout'
 import styled from '@emotion/styled'
 import { alphaSortArrayAscending } from '../utils'
@@ -13,6 +14,10 @@ import {
   List,
   ListItem,
   ListIcon,
+  Grid,
+  GridItem,
+  Heading,
+  Text,
 } from '@chakra-ui/react'
 import Section from '../components/Section'
 import { FaFilePdf, FaRegCalendarAlt } from 'react-icons/fa'
@@ -58,31 +63,84 @@ export default function Home({ curricula }) {
     <>
       <Head>
         <title>LBPS Curriculum and Instruction</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <Section id='top'>
-          <h2>Introductory Heading</h2>
-          <p>
-            Introductory paragraph explaining how this site is organized. Lorem
-            ipsum dolor sit amet consectetur adipisicing elit. Sit
-            exercitationem laborum consectetur voluptas recusandae, ab, quo
-            voluptates odio ad nisi quod, repudiandae ex iure delectus molestiae
-            eum quidem placeat unde.
-          </p>
-        </Section>
-        <Section>
-          <h2>Jump to a Grade Span</h2>
-          <NavWrapper>
-            {spans.map((span) => {
-              return (
-                <Link key={span} href={`#${span}`} passHref>
-                  <ButtonLink>{span}</ButtonLink>
-                </Link>
-              )
-            })}
-          </NavWrapper>
-        </Section>
+        <Grid
+          minH="250px"
+          templateRows={{ lg: 'repeat(3, 1fr)' }}
+          templateColumns={{ lg: 'repeat(5, 1fr)' }}
+          justifyItems="center"
+          padding="4"
+        >
+          <GridItem
+            paddingBottom={{ base: 2, lg: 4 }}
+            colSpan={{ lg: 2 }}
+            rowSpan={{ lg: 2 }}
+            alignSelf="end"
+          >
+            <Image
+              src="/images/curriculum_logo.png"
+              alt="LBPS Seal"
+              width={300}
+              height={300}
+            />
+          </GridItem>
+          <GridItem colSpan={{ lg: 3 }} rowSpan="1" alignSelf="end">
+            <Heading
+              borderBottom="2px"
+              borderColor="brand.300"
+              paddingBottom="3"
+            >
+              Introductory Heading
+            </Heading>
+          </GridItem>
+          <GridItem colSpan={{ lg: 3 }} rowSpan="1">
+            <Text
+              fontSize={['md', 'lg', 'xl', '2xl']}
+              maxW={['2xl', '3xl', '4xl']}
+              padding="4"
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi odit
+              laboriosam ratione ipsam quaerat, sapiente porro, alias tempora
+              officiis est fugit tempore magnam deleniti nesciunt veritatis
+              velit quisquam sint rerum?
+            </Text>
+          </GridItem>
+          <GridItem colSpan={{ lg: 2 }} rowspan="1">
+            <Heading
+              as="h3"
+              fontSize={'2xl'}
+              textAlign="center"
+              color="brand.200"
+              borderBottom={{ base: '2px', lg: '0px' }}
+              borderColor="brand.300"
+              marginBottom={{ base: 4, lg: 0 }}
+            >
+              Nicole Esposito
+              <br /> Assistant Superintendent <br /> of Curriculum &amp;
+              Instruction
+            </Heading>
+          </GridItem>
+          <GridItem colSpan={{ lg: 3 }} rowSpan="1">
+            <Text textAlign="center" fontSize={['md', 'lg', 'xl']}>
+              <Heading as="h4" fontSize="xl" marginBottom="4">
+                Office Information
+              </Heading>
+              <address>
+                540 Broadway <br />
+                Long Branch, NJ 07740
+              </address>
+              <br />
+              <span style={{ fontWeight: 600 }}>Maria Graziano</span>
+              <br />
+              Confidential Secretary
+              <br />
+              732-571-2868 ext. 40240
+            </Text>
+          </GridItem>
+        </Grid>
+
         {spans.map((span, i) => {
           let allAreas = []
           curricula.forEach((curriculum) => {
@@ -111,7 +169,7 @@ export default function Home({ curricula }) {
                           <AccordionItem key={areaCurriculum.title}>
                             <h4>
                               <AccordionButton>
-                                <Box flex='1' textAlign='left'>
+                                <Box flex="1" textAlign="left">
                                   {areaCurriculum.title}
                                 </Box>
                                 <AccordionIcon />
@@ -124,8 +182,8 @@ export default function Home({ curricula }) {
                                     <ListIcon as={FaFilePdf} />
                                     <a
                                       href={areaCurriculum.guide}
-                                      target='_blank'
-                                      rel='noreferrer'
+                                      target="_blank"
+                                      rel="noreferrer"
                                     >
                                       Curriculum Guide
                                     </a>
@@ -136,8 +194,8 @@ export default function Home({ curricula }) {
                                     <ListIcon as={FaRegCalendarAlt} />
                                     <a
                                       href={areaCurriculum.calendar}
-                                      target='_blank'
-                                      rel='noreferrer'
+                                      target="_blank"
+                                      rel="noreferrer"
                                     >
                                       Pacing Calendar
                                     </a>
