763ced12afa810512f40b197750b0daaa27eccd2
Neil Mastroianni <nmastroianni@longbranch.k12.nj.us>
2022-07-12 09:58:51 -0400

    using chakra heading

--- components/SectionHeader/index.js
+++ components/SectionHeader/index.js
@@ -1,17 +1,13 @@
 import styled from '@emotion/styled'
+import { Heading } from '@chakra-ui/react'
 
 const Header = styled.header`
   background-color: var(--dark-green);
   padding: 0.7rem 0;
-  h2 {
-    color: white;
-    text-align: center;
-    font-size: 1.3rem;
-  }
+
   @media (min-width: 640px) {
     padding: 0.3rem;
     h2 {
-      font-size: 1.7rem;
       letter-spacing: 0.05rem;
     }
   }
@@ -20,7 +16,9 @@ const Header = styled.header`
 export default function SectionHeader({ headerText }) {
   return (
     <Header>
-      <h2>{headerText}</h2>
+      <Heading textAlign="center" color="white">
+        {headerText}
+      </Heading>
     </Header>
   )
 }
